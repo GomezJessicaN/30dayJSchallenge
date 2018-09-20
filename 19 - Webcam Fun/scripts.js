3e169ad0a3ece2db520aa724154be37535820983
Blenki <gomezjessican@gmail.com>
2018-09-19 22:04:06 -0300

    jschallenge 19/30

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -8,16 +8,15 @@ function getVideo() {
     navigator.mediaDevices
         .getUserMedia({ video: true, audio: false })
         .then(localMediaStream => {
-            console.log(localMediaStream);
-            video.src = window.URL.createObjectURL(localMediaStream);
+            video.srcObject = localMediaStream;
             video.play();
         })
         .catch(err => {
             console.error(`OH NO! maldita sea`, err);
-        })
+        });
 }
 
-function paintToCanavas() {
+function paintToCanvas() {
     const width = video.videoWidth;
     const height = video.videoHeight;
     canvas.width = width;
@@ -27,46 +26,41 @@ function paintToCanavas() {
         ctx.drawImage(video, 0, 0, width, height);
         //take the pixels out
         let pixels = ctx.getImageData(0, 0, width, height);
-        //mess with them
         // pixels = redEffect(pixels);
-        pixels = rgbSplit(pixels);
-        ctx.globalAlpha = 0.8;
-
-        //pixels = greenScreen(pixels);
-        //put them back
+        //pixels = rgbSplit(pixels);
+        //ctx.globalAlpha = 0.8;
+        pixels = greenScreen(pixels);
         ctx.putImageData(pixels, 0, 0);
     }, 16);
 }
 
 function takePhoto() {
-
-    //played the sound
+    //sonidito
     snap.currentTime = 0;
     snap.play();
 
-    //take the data out of the canvas
     const data = canvas.toDataURL('image/jpeg');
     const link = document.createElement('a');
     link.href = data;
-    link.setAttribute('download', 'handsome');
-    link.innerHTML = `<img src="${data}" alt="Handsome Man" />`;
+    link.setAttribute('download', 'IUPIIIII');
+    link.innerHTML = `<img src="${data}" alt="MIRA UNA FOTO MIRA" />`;
     strip.insertBefore(link, strip.firstChild);
 }
 
 function redEffect(pixels) {
     for (let i = 0; i < pixels.data.length; i += 4) {
-        pixels.data[i + 0] = pixels.data[i + 0] + 100; //RED
-        pixels.data[i + 1] = pixels.data[i + 1] - 50; //GREEN
-        pixels.data[i + 2] = pixels.data[i + 2] * 0.5; //BLUE
+        pixels.data[i + 0] = pixels.data[i + 0] + 100; //Rojo
+        pixels.data[i + 1] = pixels.data[i + 1] - 50; //Verde
+        pixels.data[i + 2] = pixels.data[i + 2] * 0.5; //Azul
     }
     return pixels;
 }
 
 function rgbSplit(pixels) {
     for (let i = 0; i < pixels.data.length; i += 4) {
-        pixels.data[i - 150] = pixels.data[i + 0]; //RED
-        pixels.data[i + 100] = pixels.data[i + 1]; //GREEN
-        pixels.data[i - 150] = pixels.data[i + 2]; //BLUE
+        pixels.data[i - 150] = pixels.data[i + 0]; //Rojo
+        pixels.data[i + 100] = pixels.data[i + 1]; //Verde
+        pixels.data[i - 150] = pixels.data[i + 2]; //Azul como el mar azul
     }
     return pixels;
 }
@@ -84,13 +78,12 @@ function greenScreen(pixels) {
         blue = pixels.data[i + 2];
         alpha = pixels.data[i + 3];
 
-        if (red >= levels.rmin &&
-            green >= levels.gmin &&
-            blue >= levels.bmin &&
-            red <= levels.rmax &&
-            green <= levels.gmax &&
-            blue <= levels.bmax) {
-            //take it out
+        if (red >= levels.rmin 
+            && green >= levels.gmin 
+            && blue >= levels.bmin 
+            && red <= levels.rmax 
+            && green <= levels.gmax 
+            && blue <= levels.bmax) {
             pixels.data[i + 3] = 0;
         }
     }
@@ -99,6 +92,6 @@ function greenScreen(pixels) {
 
 getVideo();
 
-video.addEventListener('canplay', paintToCanavas);
+video.addEventListener('canplay', paintToCanvas);
 
 //http://localhost:3000/
